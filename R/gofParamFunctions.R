9b9b121131aacdb24231f018a854640dc92467ba
nikeis <nico1.keiser@gmail.com>
2023-04-20 16:07:51 +0200

    adding "getIndegree" to gofParamFunctions

--- R/gofParamFunctions.R
+++ R/gofParamFunctions.R
@@ -1,6 +1,16 @@
 ########## gofParamFunctions
 
 
+# getIndegree
+getIndegree <- function(cache, dep.var, lvls, ...){
+  m <- cache[[dep.var]]$valuedNetwork
+  v <- cbind(lvls, 0)
+  existing_v <- as.numeric(names(table(colSums(m))))+1
+  v[existing_v[existing_v %in% lvls],2] <- table(colSums(m))[existing_v %in% lvls]
+  v[,2]
+}
+
+
 # getTieWeights
 getTieWeights <- function(cache, dep.var, lvls, ...) {
   m <- cache[[dep.var]]$valuedNetwork

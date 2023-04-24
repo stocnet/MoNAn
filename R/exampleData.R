b603e9de6ecb132388d58733eb3ccdd9e8a676f7
nikeis <nico1.keiser@gmail.com>
2023-04-24 16:41:10 +0200

    upload "exampleData.rda" => generating description/manual of the exemplary data does not work atm

---
+++ R/exampleData.R
@@ -0,0 +1,12 @@
+#' Example data for MoNAn
+#'
+#' Example data containing...
+#'
+#' @format description
+#' \itemize{
+#'   \item mobilityEdgelist. blabla
+#'   \item nodeVarCat. blabla
+#'   \item nodeVarCont. blabla
+#'   \item resVarCat. blabla
+#' }
+"exampleData"

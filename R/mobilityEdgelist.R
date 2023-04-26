02ed2cb514a74c3cf3101d6838e56b5ceebbda15
nikeis <nico1.keiser@gmail.com>
2023-04-26 14:01:39 +0200

    delete "exampleData.rda" and upload example objects as single files in order to document them properly

---
+++ R/mobilityEdgelist.R
@@ -0,0 +1,34 @@
+#' Example Data for the MoNAn Package
+#'
+#' @description
+#' These are example data for the MoNAn package and can be used to estimate a 
+#' mobility network. The following objects are provided for this purpose:
+#'
+#' \describe{
+#'   \item{mobilityEdgelist}{blabla}
+#'   \item{nodeVarCat}{blabla}
+#'   \item{nodeVarCont}{blabla}
+#'   \item{resVarCat}{blabla}
+#' }
+#' 
+#' @format \code{mobilityEdgelist}
+#' A data frame with 743 rows and 2 columns.
+"mobilityEdgelist"
+
+
+#' @rdname mobilityEdgelist
+#' @format \code{nodeVarCat}
+#' An object with 17 values.
+"nodeVarCat"
+
+
+#' @rdname mobilityEdgelist
+#' @format \code{nodeVarCont}
+#' An object with 17 values.
+"nodeVarCont"
+
+
+#' @rdname mobilityEdgelist
+#' @format \code{resVarCat}
+#' An object with 742 values.
+"resVarCat"

07827bf97325928b6d375a3c5b9a9bf866bd320f
nikeis <nico1.keiser@gmail.com>
2023-05-19 12:39:17 +0200

    restyling gofParamFunctions

--- R/gofParamFunctions.R
+++ R/gofParamFunctions.R
@@ -4,31 +4,31 @@
 # getIndegree
 #' Title
 #'
-#' @param cache 
-#' @param dep.var 
-#' @param lvls 
-#' @param ... 
+#' @param cache
+#' @param dep.var
+#' @param lvls
+#' @param ...
 #'
 #' @return
 #' @export
 #'
 #' @examples
-getIndegree <- function(cache, dep.var, lvls, ...){
+getIndegree <- function(cache, dep.var, lvls, ...) {
   m <- cache[[dep.var]]$valuedNetwork
   v <- cbind(lvls, 0)
-  existing_v <- as.numeric(names(table(colSums(m))))+1
-  v[existing_v[existing_v %in% lvls],2] <- table(colSums(m))[existing_v %in% lvls]
-  v[,2]
+  existing_v <- as.numeric(names(table(colSums(m)))) + 1
+  v[existing_v[existing_v %in% lvls], 2] <- table(colSums(m))[existing_v %in% lvls]
+  v[, 2]
 }
 
 
 # getTieWeights
 #' Title
 #'
-#' @param cache 
-#' @param dep.var 
-#' @param lvls 
-#' @param ... 
+#' @param cache
+#' @param dep.var
+#' @param lvls
+#' @param ...
 #'
 #' @return
 #' @export
@@ -36,8 +36,9 @@ getIndegree <- function(cache, dep.var, lvls, ...){
 #' @examples
 getTieWeights <- function(cache, dep.var, lvls, ...) {
   m <- cache[[dep.var]]$valuedNetwork
-  if (is.null(lvls))
+  if (is.null(lvls)) {
     lvls <- 0:max(m)
+  }
   v <- cbind(lvls, 0)
   existing_v <- as.numeric(names(table(m))) + 1
   v[existing_v[existing_v %in% lvls], 2] <-

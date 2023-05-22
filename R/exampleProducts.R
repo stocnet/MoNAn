e948ea74523232a91884ebb0d6dd539b1b74a189
nikeis <nico1.keiser@gmail.com>
2023-05-22 15:20:54 +0200

    create a help file for the example products

---
+++ R/exampleProducts.R
@@ -0,0 +1,44 @@
+#' Example Products for the MoNAn Package
+#'
+#' @name exampleProducts
+#' @description
+#' These are example products for the MoNAn package and can be used in order not 
+#' to run all precedent functions and thus save time. The following products are
+#' provided:
+#'
+#' \describe{
+#'   \item{exampleState}{blabla}
+#'   \item{exampleDependentVariable}{blabla}
+#'   \item{exampleCache}{blabla}
+#'   \item{exampleEffects}{blabla}
+#'   \item{exampleResDN}{blabla}
+#' }
+#' 
+#' @rdname exampleProducts
+#' @format `exampleState`
+#' An object of class processState.monan.
+"exampleState"
+
+
+#' @rdname exampleProducts
+#' @format `exampleDependentVariable`
+#' An object of class character.
+"exampleDependentVariable"
+
+
+#' @rdname exampleProducts
+#' @format `exampleCache`
+#' An object of class list.
+"exampleCache"
+
+
+#' @rdname exampleProducts
+#' @format `exampleEffects`
+#' An object of class "effectsList.monan".
+"exampleEffects"
+
+
+#' @rdname exampleProducts
+#' @format `exampleResDN`
+#' An object of class "result.monan".
+"exampleResDN"

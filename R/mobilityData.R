5b268b5aa6e621169cdabaad03313d3784de50a2
nikeis <nico1.keiser@gmail.com>
2023-06-07 14:32:37 +0200

    change exampleData to mobilityData

---
+++ R/mobilityData.R
@@ -0,0 +1,36 @@
+#' Example Data for the MoNAn Package
+#'
+#' @name mobilityData
+#' @description
+#' These are example data for the MoNAn package and can be used to estimate a 
+#' mobility network. The following objects are provided for this purpose:
+#'
+#' \describe{
+#'   \item{mobilityEdgelist}{blabla}
+#'   \item{nodeVarCat}{blabla}
+#'   \item{nodeVarCont}{blabla}
+#'   \item{resVarCat}{blabla}
+#' }
+#' 
+#' @rdname mobilityData
+#' @format `mobilityEdgelist`
+#' A data frame with 743 rows and 2 columns.
+"mobilityEdgelist"
+
+
+#' @rdname mobilityData
+#' @format `nodeVarCat`
+#' An object with 17 values.
+"nodeVarCat"
+
+
+#' @rdname mobilityData
+#' @format `nodeVarCont`
+#' An object with 17 values.
+"nodeVarCont"
+
+
+#' @rdname mobilityData
+#' @format `resVarCat`
+#' An object with 742 values.
+"resVarCat"

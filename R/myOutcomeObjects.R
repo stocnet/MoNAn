dbbe752debe847826e928cdfb2d97e30099b0bcf
nikeis <nico1.keiser@gmail.com>
2023-06-07 14:13:54 +0200

    adjust documentation for myOutcomeObjects

---
+++ R/myOutcomeObjects.R
@@ -0,0 +1,44 @@
+#' Exemplary Outcome Objects for the MoNAn Package
+#'
+#' @name myOutcomeObjects
+#' @description
+#' These are exemplary outcome objects for the MoNAn package and can be used in 
+#' order not to run all precedent functions and thus save time. The following 
+#' products are provided:
+#'
+#' \describe{
+#'   \item{myState}{blabla}
+#'   \item{myDependentVariable}{blabla}
+#'   \item{myCache}{blabla}
+#'   \item{myEffects}{blabla}
+#'   \item{myResDN}{blabla}
+#' }
+#' 
+#' @rdname myOutcomeObjects
+#' @format `myState`
+#' An object of class "processState.monan".
+"myState"
+
+
+#' @rdname myOutcomeObjects
+#' @format `myDependentVariable`
+#' An object of class "character".
+"myDependentVariable"
+
+
+#' @rdname myOutcomeObjects
+#' @format `myCache`
+#' An object of class "list".
+"myCache"
+
+
+#' @rdname myOutcomeObjects
+#' @format `myEffects`
+#' An object of class "effectsList.monan".
+"myEffects"
+
+
+#' @rdname myOutcomeObjects
+#' @format `myResDN`
+#' An object of class "result.monan".
+"myResDN"

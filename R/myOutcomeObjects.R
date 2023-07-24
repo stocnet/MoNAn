7bc371de6992b678a1509227110c205d762d07be
nikeis <nico1.keiser@gmail.com>
2023-07-24 16:47:46 +0200

    add mySimDN to myOutcomeObjects

--- R/myOutcomeObjects.R
+++ R/myOutcomeObjects.R
@@ -5,47 +5,44 @@
 #' These are exemplary outcome objects for the MoNAn package and can be used in 
 #' order not to run all precedent functions and thus save time. The following 
 #' products are provided:
-#'
-#' \describe{
-#'   \item{myState}{blabla}
-#'   \item{myDependentVariable}{blabla}
-#'   \item{myCache}{blabla}
-#'   \item{myEffects}{blabla}
-#'   \item{myAlg}{blabla}
-#'   \item{myResDN}{blabla}
-#' }
 #' 
 #' @rdname myOutcomeObjects
 #' @format `myState`
-#' An object of class "processState.monan".
+#' An object of class `processState.monan` created by the function [createProcessState()].
 "myState"
 
 
 #' @rdname myOutcomeObjects
 #' @format `myDependentVariable`
-#' An object of class "character".
+#' An object of class `character`.
 "myDependentVariable"
 
 
 #' @rdname myOutcomeObjects
 #' @format `myCache`
-#' An object of class "list".
+#' An object of class `list` created by the function [createWeightedCache()].
 "myCache"
 
 
 #' @rdname myOutcomeObjects
 #' @format `myEffects`
-#' An object of class "effectsList.monan".
+#' An object of class `effectsList.monan` created by the function [createEffectsObject()].
 "myEffects"
 
 
 #' @rdname myOutcomeObjects
 #' @format `myAlg`
-#' An object of class "algorithm.monan".
+#' An object of class `algorithm.monan` created by the function [createAlgorithm()].
 "myAlg"
 
 
 #' @rdname myOutcomeObjects
 #' @format `myResDN`
-#' An object of class "result.monan".
+#' An object of class `result.monan` created by the function [estimateMobilityNetwork()].
 "myResDN"
+
+
+#' @rdname myOutcomeObjects
+#' @format `mySimDN`
+#' An object of class `sims.monan` created by the function [simulateMobilityNetworks()].
+"mySimDN"

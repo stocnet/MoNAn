a98315d3088b18321ec89464d1a668ee379ae06f
nikeis <nico1.keiser@gmail.com>
2023-08-08 10:27:04 +0200

    update documentation of myOutcomeObjects

--- R/myOutcomeObjects.R
+++ R/myOutcomeObjects.R
@@ -7,36 +7,42 @@
 #' products are provided:
 #' 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `myState`
 #' An object of class `processState.monan` created by the function [createProcessState()].
 "myState"
 
 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `myCache`
 #' A `list` created by the function [createWeightedCache()].
 "myCache"
 
 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `myEffects`
 #' An object of class `effectsList.monan` created by the function [createEffectsObject()].
 "myEffects"
 
 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `myAlg`
 #' An object of class `algorithm.monan` created by the function [createAlgorithm()].
 "myAlg"
 
 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `myResDN`
 #' An object of class `result.monan` created by the function [estimateMobilityNetwork()].
 "myResDN"
 
 
 #' @rdname myOutcomeObjects
+#' @usage NULL
 #' @format `mySimDN`
 #' An object of class `sims.monan` created by the function [simulateMobilityNetworks()].
 "mySimDN"

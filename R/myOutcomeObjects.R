cf19e908b926bf03330b5a07454f8ba26e16c8de
nikeis <nico1.keiser@gmail.com>
2023-08-08 11:38:20 +0200

    update documentation of myOutcomeObjects

--- R/myOutcomeObjects.R
+++ R/myOutcomeObjects.R
@@ -9,40 +9,40 @@
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `myState`
-#' An object of class `processState.monan` created by the function [createProcessState()].
+#' An object of class "processState.monan" created by the function [createProcessState()].
 "myState"
 
 
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `myCache`
-#' A `list` created by the function [createWeightedCache()].
+#' A "list" created by the function [createWeightedCache()].
 "myCache"
 
 
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `myEffects`
-#' An object of class `effectsList.monan` created by the function [createEffectsObject()].
+#' An object of class "effectsList.monan" created by the function [createEffectsObject()].
 "myEffects"
 
 
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `myAlg`
-#' An object of class `algorithm.monan` created by the function [createAlgorithm()].
+#' An object of class "algorithm.monan" created by the function [createAlgorithm()].
 "myAlg"
 
 
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `myResDN`
-#' An object of class `result.monan` created by the function [estimateMobilityNetwork()].
+#' An object of class "result.monan" created by the function [estimateMobilityNetwork()].
 "myResDN"
 
 
 #' @rdname myOutcomeObjects
 #' @usage NULL
 #' @format `mySimDN`
-#' An object of class `sims.monan` created by the function [simulateMobilityNetworks()].
+#' An object of class "sims.monan" created by the function [simulateMobilityNetworks()].
 "mySimDN"

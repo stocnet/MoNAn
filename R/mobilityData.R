e11de1be2296ecf389b7ab1903e4293c39199b8b
nikeis <nico1.keiser@gmail.com>
2023-08-08 10:26:23 +0200

    update documentation of mobilityData

--- R/mobilityData.R
+++ R/mobilityData.R
@@ -6,31 +6,40 @@
 #' mobility network. The following objects are provided for this purpose:
 #'
 #' \describe{
-#'   \item{mobilityEdgelist}{blabla}
-#'   \item{orgRegion}{blabla}
-#'   \item{orgSize}{blabla}
-#'   \item{indSex}{blabla}
+#'   \item{`mobilityEdgelist`}{This data frame represents the origin at time 1 
+#'   (first column) as well as the destination at time 2 (second column) for 
+#'   each of the 742 individuals between the 17 organisations. Individuals could
+#'   also stay in their organisation meaning that origin equals destination.}
+#'   \item{`orgRegion`}{This characteristic describes whether the resp. 
+#'   organisation is located on the northern (1) or southern (0) island.}
+#'   \item{`orgSize`}{This composite measure represents the size of each 
+#'   organisation including its number of workers, assets, and revenue.}
+#'   \item{`indSex`}{This characteristic represents each individual's gender.}
 #' }
 #' 
 #' @rdname mobilityData
+#' @usage NULL
 #' @format `mobilityEdgelist`
 #' A data frame with 743 rows and 2 columns.
 "mobilityEdgelist"
 
 
 #' @rdname mobilityData
+#' @usage NULL
 #' @format `orgRegion`
 #' An object with 17 values.
 "orgRegion"
 
 
 #' @rdname mobilityData
+#' @usage NULL
 #' @format `orgSize`
 #' An object with 17 values.
 "orgSize"
 
 
 #' @rdname mobilityData
+#' @usage NULL
 #' @format `indSex`
 #' An object with 742 values.
 "indSex"
